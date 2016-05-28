daed2eb14e1fc0a2ee5ad044f439891dd6b84996
Shea Norwood <sheanorwood@msn.comgit config --global user.email sheanorwood@msn.com>
2016-05-28 02:56:14 +0000

    Build Sections

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ devise_for :users
 root  'static_pages#index'
 resources :courses, only: [:index, :show]
 namespace :instructor do
-    resources :courses, only: [:new, :create, :show]
-end
+    resources :courses, only: [:new, :create, :show] do
+        resources  :sections, only: [:new, :create]
+        end
+    end
 end
